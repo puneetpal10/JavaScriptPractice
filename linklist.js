270d0ef63119f21040c87a5ae92c1c3c7d28b32a
puneetpal10 <puneet09876@gmail.com>
2021-08-01 21:58:19 +0530

    added linklist

--- linklist.js
+++ linklist.js
@@ -1,4 +1,12 @@
 
+
+// class Node{
+//     constructor(value){
+//         this.value = value;
+//         this.next = null
+//     }
+// }
+
 class LinkList {
     constructor(value) {
         this.head = {
@@ -35,11 +43,61 @@ class LinkList {
         return this;
 
     }
+
+
+    printList(){
+        const array = [];
+        let currentNode = this.head;
+        while (currentNode !== null){
+            array.push(currentNode.value);
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+    insert(index,value){
+
+        //check parameters
+
+        //If enterd index is greater than this list, add that at the end of list.
+        if (index >= this.length){
+            this.append(value);
+        }
+
+        const newNode = {
+            value: value,
+            next: null
+        };
+
+        const leader = this.traverseToIndex(index - 1);
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length ++;
+        return this.printList();
+    }
+
+    traverseToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while(counter !== index){
+            currentNode = currentNode.next;
+            counter++;
+        }
+        return currentNode;
+    }
 }
 
 
+
 const myLL = new LinkList(10);
-// myLL.append(5)
-// myLL.append(15)
-myLL.prepend(1)
-console.log(myLL);
+myLL.append(5);
+myLL.append(15);
+myLL.prepend(1);
+// console.log(myLL);
+
+
+myLL.insert(2,199);
+myLL.insert(4,299);
+
+console.log(myLL.printList());
