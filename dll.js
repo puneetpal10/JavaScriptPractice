ba74b1b434448e025ba6b0ea094589b7c84c7fa4
puneetpal10 <puneet09876@gmail.com>
2021-08-02 21:25:52 +0530

    dll

--- dll.js
+++ dll.js
@@ -7,12 +7,12 @@
 //     }
 // }
 
-class LinkList {
+class DoublyLinkList {
     constructor(value) {
         this.head = {
             value: value,
             next: null,
-
+            prev: null
         }
         this.tail = this.head;
         this.length = 1;
@@ -22,8 +22,10 @@ class LinkList {
 
         const newNode = {
             value: value,
-            next: null
+            next: null,
+            prev: null
         }
+        newNode.prev = this.tail;
         this.tail.next = newNode;
         this.tail = newNode;
         this.length++;
@@ -35,9 +37,11 @@ class LinkList {
     prepend(value) {
         const newNode = {
             value: value,
-            next: null
+            next: null,
+            prev: null
         }
         newNode.next = this.head;
+        this.head.prev = newNode;
         this.head = newNode;
         this.length++;
 
@@ -67,13 +71,17 @@ class LinkList {
 
         const newNode = {
             value: value,
-            next: null
+            next: null,
+            prev: null
         };
 
         const leader = this.traverseToIndex(index - 1);
-        const holdingPointer = leader.next;
+        const follower = leader.next;
         leader.next = newNode;
-        newNode.next = holdingPointer;
+        newNode.prev = leader;
+        newNode.next = follower;
+        follower.prev = newNode;
+
         this.length ++;
         return this.printList();
     }
@@ -100,20 +108,48 @@ class LinkList {
         return this.printList();
 
     }
+
+
+    reverse(){
+        if (!this.head.next){
+            return this.head;
+        }
+        let first = this.head;
+        this.tail = this.head;
+        let second = first.next;
+
+
+        while(second){
+            const temp = second.next;
+            second.next = first;
+            first = second;
+            second = temp;
+        }
+        this.head.next = null;
+        this.head = first;
+
+        return this;
+    }
 }
 
 
 
-const myLL = new LinkList(10);
+const myLL = new DoublyLinkList(10);
 myLL.append(5);
 myLL.append(15);
 myLL.prepend(1);
 // console.log(myLL);
 
 
-myLL.insert(2,199);
-myLL.insert(4,299);
+// myLL.insert(2,199);
+// myLL.insert(4,299);
 
 
-myLL.remove(2);
+// myLL.remove(2);
+
+console.log(myLL);
 console.log(myLL.printList());
+
+myLL.reverse();
+console.log(myLL.printList());
+
